8cd4df5370c3157a4e3d4b94a2ab3f091097933e
Alex <alex.h.virtanen@gmail.com>
2017-01-08 09:24:47 +0200

    messy UI work..

---
+++ MineSweeper/src/main/java/asd/fgh/minesweeper/ui/StartFrame.java
@@ -0,0 +1,12 @@
+package asd.fgh.minesweeper.ui;
+
+import java.awt.Frame;
+
+
+public class StartFrame extends Frame {
+
+    public StartFrame() {
+        //TODO: game types + custom
+    }
+    
+}
